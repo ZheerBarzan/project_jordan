6872bdbc434b13436df51a5661691b89ad423c17
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-04 03:22:22 +0300

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_jordan/pages/login_or_registation.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(const MyApp());
