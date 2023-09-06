0edf39933705f9759dc21c3168b3cfd0ca206a44
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-06 03:18:37 +0300

    added the bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:http/http.dart' as http;
 import 'package:project_jordan/model/Teams.dart';
 
@@ -31,6 +33,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(1023, 20, 68, 144),
+        title: Text(
+          "NBA",
+          style: GoogleFonts.bebasNeue(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
         actions: [
           IconButton(
               icon: const Icon(Icons.logout), onPressed: () => signOut()),
@@ -57,6 +67,68 @@ class HomePage extends StatelessWidget {
             );
           }
         },
+      ),
+      bottomNavigationBar: Container(
+        color: const Color.fromARGB(1023, 20, 68, 144),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: GNav(
+            backgroundColor: const Color.fromARGB(1023, 20, 68, 144),
+            gap: 8,
+            onTabChange: (value) {},
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            tabs: [
+              GButton(
+                borderRadius: BorderRadius.circular(25),
+                icon: Icons.newspaper,
+                backgroundColor: Colors.red,
+                iconActiveColor: Colors.white,
+                iconColor: Colors.white,
+                text: "news",
+                textStyle: GoogleFonts.bebasNeue(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              GButton(
+                borderRadius: BorderRadius.circular(25),
+                icon: Icons.scoreboard_outlined,
+                backgroundColor: Colors.red,
+                iconActiveColor: Colors.white,
+                iconColor: Colors.white,
+                text: "score",
+                textStyle: GoogleFonts.bebasNeue(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              GButton(
+                borderRadius: BorderRadius.circular(25),
+                icon: Icons.stacked_bar_chart_rounded,
+                backgroundColor: Colors.red,
+                iconActiveColor: Colors.white,
+                iconColor: Colors.white,
+                text: "stats",
+                textStyle: GoogleFonts.bebasNeue(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              GButton(
+                borderRadius: BorderRadius.circular(25),
+                icon: Icons.account_box_rounded,
+                backgroundColor: Colors.red,
+                iconActiveColor: Colors.white,
+                iconColor: Colors.white,
+                text: "profile",
+                textStyle: GoogleFonts.bebasNeue(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
