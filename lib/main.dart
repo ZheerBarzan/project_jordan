aa462115e608dd5ce5a32cb9ce62ae0868cea1a0
shadyar-bakr <shadyar.bakr@proton.me>
2023-09-12 18:05:58 +0300

    refactor: :recycle:

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,5 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      
-      debugShowCheckedModeBanner: false,
-      home: Authpage(),
-    );
-  }
+  runApp(const App());
 }
