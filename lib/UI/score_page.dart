6b09cb7d7463384531e3c0e33fe1f795622edaa7
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-15 03:25:22 +0300

    established communication with the NBA api

--- lib/UI/score_page.dart
+++ lib/UI/score_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:project_jordan/components/game_List.dart';
 import 'package:project_jordan/model/teams.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,52 +13,10 @@ class ScorePage extends StatefulWidget {
 }
 
 class _ScorePageState extends State<ScorePage> {
-  List<Team> teams = [];
-
-  Future getTeams() async {
-    var response = await http.get(Uri.https("balldontlie.io", "api/v1/teams"));
-    var jsonData = jsonDecode(response.body);
-
-    for (var eachTeam in jsonData['data']) {
-      final team = Team(
-          id: eachTeam['id'],
-          abbreviation: eachTeam['abbreviation'],
-          city: eachTeam['city'],
-          conference: eachTeam['conference'],
-          division: eachTeam['division'],
-          fullName: eachTeam['full_name'],
-          name: eachTeam['name']);
-      teams.add(team);
-    }
-  }
-
-  Future getPlayers() async {}
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder(
-        future: getTeams(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.builder(
-                itemCount: teams.length,
-                itemBuilder: (
-                  context,
-                  index,
-                ) {
-                  return ListTile(
-                    title: Text(
-                        "${teams[index].abbreviation}, ${teams[index].city}"),
-                  );
-                });
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
+    return const Scaffold(
+      body: GameList(),
     );
   }
 }
--- lib/services/nba_api.dart
+++ lib/services/nba_api.dart
@@ -6,25 +6,24 @@ import 'package:http/http.dart' as http;
 class NbaAPI {
   const NbaAPI();
 
-  static const baseUrl = "https://www.balldontlie.io/api/v1/games/";
+  static const baseUrl = "https://www.balldontlie.io/api/v1/games";
 
-  Future<List<Game>> fetchArticles(String catagory) async {
+  Future<List<Game>> fetchGames(int year) async {
     var url = NbaAPI.baseUrl;
+    url += "?seasons[]=${year.toString()}";
 
     final response = await http.get(Uri.parse(url));
+
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body);
-      if (json['status'] == "ok") {
-        final dynamic articleJSON = json['articles'] ?? [];
-        final List<Game> articles = articleJSON.map<Game>((e) {
-          return Game.fromJson(e);
-        }).toList();
-        return articles;
-      } else {
-        throw Exception(json['messege'] ?? 'Failed to load ');
-      }
+
+      final dynamic articleJSON = json['data'] ?? [];
+      final List<Game> articles = articleJSON.map<Game>((e) {
+        return Game.fromJson(e);
+      }).toList();
+      return articles;
     } else {
-      throw Exception("bad respose");
+      throw Exception('Failed to load ');
     }
   }
 }
